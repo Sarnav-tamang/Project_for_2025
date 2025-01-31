3d5fc24bfc91d448526b9c8bd8fa413e21f0c5e5
Sarnav-tamang <168201872+SarnavTamang001@users.noreply.github.com>
2025-01-31 10:00:27 +0530

    Exam Admin HomePage

---
+++ examhome.js
@@ -0,0 +1,10 @@
+// Js for Toggle menu
+let navLinks = document.getElementById("nav-links");
+function ShowMenu() {
+    navLinks.style.right = "0";
+}
+function HideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+
